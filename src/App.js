379f2889be2268b216a648bd508ddd4ae244bad5
Himanshu <himanshu32288@gmail.com>
2021-12-26 18:35:22 +0530

    initialisation

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Intro from './components/intro/Intro';
+
+function App() {
+  return (
+    <Intro />
+  );
+}
+
+export default App;
---
+++ src/components/intro/Intro.js
@@ -0,0 +1,11 @@
+import './Intro.css'
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="intro-left">Left</div>
+            <div className="intro-right">Right</div>
+        </div>
+    )
+}
+
+export default Intro;
