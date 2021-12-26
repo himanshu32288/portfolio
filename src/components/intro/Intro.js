98c19f77d341315f93212157f80145b114126d71
Himanshu <himanshu32288@gmail.com>
2021-12-26 19:43:36 +0530

    added intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,10 +1,34 @@
 import './Intro.css'
+import Me from '../../img/pp.png'
 const Intro = () => {
     return (
-        <div className="intro">
-            <div className="intro-left">Left</div>
-            <div className="intro-right">Right</div>
-        </div>
+        <>
+            <div className="intro">
+                <div className="intro-left">
+                    <div className="i-left-wrapper">
+                        <h2 className="i-intro">Hello,My name is</h2>
+                        <h1 className='i-name'>Himanshu Kumar</h1>
+                        <div className="i-title">
+                            <div className="i-title-wrapper">
+                                <div className="i-title-item">Web Devloper</div>
+                                <div className="i-title-item">Virtual Reality Beginner</div>
+                                <div className="i-title-item">UI/UX Designer</div>
+                                <div className="i-title-item">DSA</div>
+
+                            </div>
+                        </div>
+                        <p className="i-desc">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi quibusdam fugit aut necessitatibus exercitationem eligendi velit facere explicabo molestiae voluptate repellat, sit cupiditate minima, inventore odit ut dicta! Repellendus, eius.
+                        </p>
+                    </div>
+
+                </div>
+                <div className="intro-right">
+                    <div className="i-bg"></div>
+                    <img src={Me} alt="" className="i-image" />
+                </div>
+            </div>
+        </>
     )
 }
 
