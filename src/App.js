2076be1f3baf4ac068758615314bc849c28a4250
Himanshu <himanshu32288@gmail.com>
2021-12-27 18:34:29 +0530

    added fevicon.ico and edited title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import About from './components/about/About';
-import Intro from './components/intro/Intro';
-import ProjectList from './components/project/ProjectList';
+import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Intro from "./components/intro/Intro";
+import ProjectList from "./components/project/ProjectList";
 function App() {
   return (
     <>
       <Intro />
       <About />
       <ProjectList />
+      <Contact />
     </>
   );
 }
